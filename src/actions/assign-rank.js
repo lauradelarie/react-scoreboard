f7cc4a1a3714a141203a67d33105fa2ecd4002da
lauradelar <lrdela.rie@gmail.com>
2016-11-06 12:50:46 +0100

    Add assign-rank action & function to reducer

---
+++ src/actions/assign-rank.js
@@ -0,0 +1,25 @@
+import api from '../middleware/api'
+import appLoading from './loading'
+import updatePlayers from './update-players'
+
+export const ASSIGN_RANK = 'ADD_RANK'
+
+export default (playerId) => {
+  return (dispatch) => {
+    dispatch(appLoading(true))
+
+    api.patch('players/${playerId}', playerId)
+      .then(() => {
+        assignRank(PlayerId)
+        dispatch(appLoading(false))
+        dispatch(updatePlayers(true))
+      })
+  }
+}
+
+export const (playerId) => {
+  return {
+    type: ASSIGN_RANK,
+    payload: playerId
+  }
+}
--- src/reducers/players.js
+++ src/reducers/players.js
@@ -1,44 +1,86 @@
-import { UPDATE_PLAYERS } from '../actions/update-players'
-import { ADD_PLAYER } from '../actions/add-player'
-import { DELETE_PLAYER } from '../actions/delete-player'
-import { PLUS_ONE } from '../actions/plus-one'
 
-export default (state = [], { type, payload } = {}) => {
+import { ADD_PLAYER } from '../actions/add-player';
+import { DELETE_PLAYER } from '../actions/delete-player';
+import { ASSIGN_RANK } from '../actions/assign-rank'
+import { PLUS_ONE } from '../actions/plus-one';
+import { SET_RANK } from '../actions/set-rank';
+
+export default (state = [], {type, payload} = {}) => {
   switch (type) {
-    case UPDATE_PLAYERS :
-      return payload
-
     case PLUS_ONE :
       return sortPlayers(state.map((player) => {
         if (player.playerId === payload) {
           return Object.assign(
-            {}, player, { points: player.points + 1 })
+            {}, player, {points: player.points + 1});
         }
-        return player
-      }))
+        return player;
+      }));
 
     case ADD_PLAYER :
-      return sortPlayers(state.concat([ payload ]))
+      const newPlayer = {
+        playerId: nextPlayerId(state),
+        name: payload,
+        avatar: `https://api.adorable.io/avatars/285/${payload}.png`,
+        points: 0
+      };
 
     case DELETE_PLAYER :
       return sortPlayers(state.filter((player) => {
-        return player.playerId != payload
-      }))
+        return player.playerId != payload;
+      }));
 
-    default:
-      return state
+    case ASSIGN_RANK :
+      const assignRank = (players) => {
+        return gold = state.filter((player) => {
+          if (player.points === players[0].points && player.points >= 10){
+            return Object.assign(
+              {}, player, { rank: 'gold'}
+            )
+          }
+        })
+
+        afterGoldPlayers = players.slice(gold.length, state.players.length)
+
+        return silver = afterGoldPlayers.filter((player) => {
+          if (player.points === afterGoldPlayers[0].points && player.points >= 10){
+            return Object.assign(
+              {}, player, { rank: 'silver'}
+            )
+          }
+        })
+
+        afterSilverPlayers = afterGoldPlayers.slice(silver.length, afterGoldPlayers.length)
+
+        return bronze = afterSilverPlayers.filter((player) => {
+          if (player.points === afterSilverPlayers[0].points && player.points >= 10){
+            return Object.assign(
+              {}, player, { rank: 'bronze'}
+            )
+          }
+        })
+
+        afterBronzePlayers = afterSilverPlayers.slice(bronze.length, afterSilverPlayers.length)
+
+        return none = afterBronzePlayers.filter((player) => {
+          if (player.points === afterBronzePlayers[0].points && player.points >= 10){
+            return Object.assign(
+              {}, player, { rank: 'none'}
+            )
+          }
+        })
+      }
   }
 }
 
 export const nextPlayerId = (players) => {
   return players.reduce((previousHighestValue, nextPlayerToCheck) => {
     return (previousHighestValue > nextPlayerToCheck.playerId) ?
-      previousHighestValue : nextPlayerToCheck.playerId
-  }, 0) + 1
-}
+      previousHighestValue : nextPlayerToCheck.playerId;
+  }, 0) + 1;
+};;
 
 export const sortPlayers = (players) => {
   return players.concat().sort((prev, next) => {
-    return next.points - prev.points
-  })
-}
+    return next.points - prev.points;
+  });
+};;
